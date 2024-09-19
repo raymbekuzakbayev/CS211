b7fc923f58e1437a867658d8b41cad996d7f9ef5
raymbekuzakbayev <r.uzakbayev@newuu.uz>
2024-09-19 15:52:23 +0500

    Update week4.java

--- week4.java
+++ week4.java
@@ -26,3 +26,72 @@ public enum UserRole {
         return this.value;
     }
 }
+
+public class BankAccount {
+    String accountNumber;
+    double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance = balance + amount;
+    }
+    public void withdraw(double amount) {
+        balance = balance - amount;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+}
+    public class Customer {
+    private String name;
+    private BankAccount bankAccount;
+
+    public Customer(String name, BankAccount bankAccount) {
+        this.name = name;
+        this.bankAccount = bankAccount;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public BankAccount getBankAccount() {
+        return bankAccount;
+    }
+}
+
+public class ATM {
+    public Customer currentCustomer;
+    ATM(Customer currentCustomer) {
+        this.currentCustomer = currentCustomer;
+    }
+    private Customer currentCustomer;
+    public String accountNumber;
+    public void login(String accountNumber) {
+        if (currentCustomer.getBankAccount().getAccountNumber().equals(accountNumber)) {}
+        System.out.println(currentCustomer.getBankAccount().getAccountNumber());
+    } else {
+        System.out.println(currentCustomer.getBankAccount().getAccountNumber());
+    }
+    public void logout() {
+        this.accountNumber = null;
+    }
+    public void deposit(double amount) {
+        currentCustomer.getBankAccount().deposit(amount);
+    }
+    public void withdraw(double amount) {
+        currentCustomer.getBankAccount().withdraw(amount);
+    }
+
+    public double checkBalance() {
+        BankAccount AccNum = this.currentCustomer.getBankAccount();
+        return AccNum.getBalance();
+    }
+}
+
